318f71e3cbaba56bb220e707438ffe0fb366de90
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-05-06 20:06:07 +0000

    #

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ function checkTheTriangle() {
   const lengthA = document.getElementById("get-lengthA").value
   const lengthB = document.getElementById("get-lengthB").value
   const lengthC = document.getElementById("get-lengthC").value
+  const lengthPack = [lengthA, lengthB, lengthC]
 
   // using the cosine law
   const angleA = Math.acos((lengthB ** 2 + lengthC ** 2 - lengthA ** 2) / (2 * lengthB * lengthC)) * (180 / Math.PI)
@@ -14,4 +15,14 @@ function checkTheTriangle() {
   const angleC = Math.acos((lengthA ** 2 + lengthB ** 2 - lengthC ** 2) / (2 * lengthA * lengthB)) * (180 / Math.PI)
 
   const sumOfAngles = Number((angleA).toFixed(2)) + Number((angleB).toFixed(2)) + Number((angleC).toFixed(2))
+  
+  if(angleA+angleB+angleC == 180) {
+    if(lengthA == lengthC == lengthB) {
+      document.getElementById("Answer").innerHTML = "This triangle is equilateral triangle triangle"
+    }else if(lengthA == lengthB || lengthB == lengthC || lengthA == lengthC) {
+      document.getElementById("Answer").innerHTML = "This triangle is isosceles triangle"
+    }else {
+      document.getElementById("Answer").innerHTML = "This triangle is scalene triangle"
+    }
+  }
 } 
